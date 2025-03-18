fd74ae57dcd6cae4a1c5909b0cf4ae248631dc0a
YejiJeonn <yejijeon02@gmail.com>
2025-03-18 09:03:28 +0900

    first commit

---
+++ src/test/java/com/example/beingus_be/BeingusBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.beingus_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeingusBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
